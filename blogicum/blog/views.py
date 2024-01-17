2e2a575adba7350b7e13aa0ee82941bf72691492
NikEvgesha <nikitenkowa.e@yandex.ru>
2024-01-17 14:28:07 +0300

    main logic

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,66 +1,40 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django.db.models import Q
 
-
-posts = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
-                страшным штормом, потерпел крушение.
-                Весь экипаж, кроме меня, утонул; я же,
-                несчастный Робинзон Крузо, был выброшен
-                полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами. Я немного приободрился,
-                хотя печаль о погибших товарищах не покидала меня.
-                Мне всё думалось, что, останься мы на корабле, мы
-                непременно спаслись бы. Теперь из его обломков мы могли бы
-                построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
-    },
-    {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер. 25 октября.  Корабль за ночь разбило
-                в щепки; на том месте, где он стоял, торчат какие-то
-                жалкие обломки,  да и те видны только во время отлива.
-                Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
-    },
-]
+from .models import Post, Category, Location
 
 
 def index(request):
     template = 'blog/index.html'
+    now = datetime.datetime.now()
+    posts = (Post.objects
+        .select_related('category')
+        .select_related('location')
+        .select_related('author')
+        .filter(pub_date__lte=now, is_published=True, category__is_published=True)
+        .order_by('-pub_date')[:5])
     context = {
-        'posts': reversed(posts)
+        'post_list': posts
     }
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
     template = 'blog/detail.html'
-    for post in posts:
-        if post.get('id') == post_id:
-            context = {'post': post}
-            return render(request, template, context)
-    return HttpResponseNotFound("<h1 style='text-align: center;"
+    now = datetime.datetime.now()
+    try:
+        post = (Post.objects
+            .select_related('category')
+            .select_related('location')
+            .select_related('author')
+            .get(pk=post_id, is_published=True, category__is_published=True, pub_date__lte=now))
+        context = {'post': post}
+        return render(request, template, context)
+    except Post.DoesNotExist:
+        return HttpResponseNotFound("<h1 style='text-align: center;"
                                 "margin-top: 10%;"
                                 "font-size: 4rem;'>"
                                 "Такого поста нет :(</h1>")
@@ -68,7 +42,21 @@ def post_detail(request, post_id):
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = {
-        'category': category_slug
-    }
-    return render(request, template, context)
+    now = datetime.datetime.now()
+    try:
+        category = Category.objects.get(slug=category_slug, is_published=True)
+        category_posts = (Post.objects
+            .select_related('category')
+            .select_related('location')
+            .select_related('author')
+            .filter(category__slug=category_slug, is_published=True, pub_date__lte=now))
+        context = {
+            'category': category.title,
+            'post_list': category_posts
+        }
+        return render(request, template, context)
+    except Category.DoesNotExist:
+        return HttpResponseNotFound("<h1 style='text-align: center;"
+                                "margin-top: 10%;"
+                                "font-size: 4rem;'>"
+                                "Такой категории постов нет :(</h1>")
