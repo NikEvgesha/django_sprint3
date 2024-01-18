c02aee39a995536b24e3ea8d6a99789834224145
NikEvgesha <nikitenkowa.e@yandex.ru>
2024-01-18 19:48:36 +0300

    review_1 fix

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,23 +1,23 @@
 import datetime
 
 from django.shortcuts import render
-from django.http import HttpResponseNotFound
+from django.shortcuts import get_object_or_404
 
 from .models import Post, Category
+
+POSTS_LIMIT = 5
 
 
 def index(request):
     template = 'blog/index.html'
     now = datetime.datetime.now()
     posts = (Post.objects
-             .select_related('category')
-             .select_related('location')
-             .select_related('author')
+             .select_related('category', 'location', 'author')
              .filter(
                  pub_date__lte=now,
                  is_published=True,
                  category__is_published=True)
-             .order_by('-pub_date')[:5])
+             .order_by('-pub_date')[:POSTS_LIMIT])
     context = {
         'post_list': posts
     }
@@ -27,45 +27,31 @@ def index(request):
 def post_detail(request, post_id):
     template = 'blog/detail.html'
     now = datetime.datetime.now()
-    try:
-        post = (Post.objects
-                .select_related('category')
-                .select_related('location')
-                .select_related('author')
-                .get(
-                    pk=post_id,
-                    is_published=True,
-                    category__is_published=True,
-                    pub_date__lte=now))
-        context = {'post': post}
-        return render(request, template, context)
-    except Post.DoesNotExist:
-        return HttpResponseNotFound("<h1 style='text-align: center;"
-                                    "margin-top: 10%;"
-                                    "font-size: 4rem;'>"
-                                    "Такого поста нет :(</h1>")
+    queryset = (Post.objects
+                .select_related('category', 'location', 'author'))
+    post = get_object_or_404(queryset,
+                             pk=post_id,
+                             is_published=True,
+                             category__is_published=True,
+                             pub_date__lte=now)
+    context = {'post': post}
+    return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
     now = datetime.datetime.now()
-    try:
-        category = Category.objects.get(slug=category_slug, is_published=True)
-        category_posts = (Post.objects
-                          .select_related('category')
-                          .select_related('location')
-                          .select_related('author')
-                          .filter(
-                              category__slug=category_slug,
-                              is_published=True,
-                              pub_date__lte=now))
-        context = {
-            'category': category.title,
-            'post_list': category_posts
-        }
-        return render(request, template, context)
-    except Category.DoesNotExist:
-        return HttpResponseNotFound("<h1 style='text-align: center;"
-                                    "margin-top: 10%;"
-                                    "font-size: 4rem;'>"
-                                    "Такой категории постов нет :(</h1>")
+    category = (get_object_or_404(Category,
+                                  slug=category_slug,
+                                  is_published=True))
+    category_posts = (Post.objects
+                      .select_related('category', 'location', 'author')
+                      .filter(
+                          category__slug=category_slug,
+                          is_published=True,
+                          pub_date__lte=now))
+    context = {
+        'category': category.title,
+        'post_list': category_posts
+    }
+    return render(request, template, context)
